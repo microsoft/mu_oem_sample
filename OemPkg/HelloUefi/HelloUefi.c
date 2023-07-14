d479c883e15187f09138bc51bd604f7af250e0d8
Doug Flick <dougflick@microsoft.com>
2023-07-13 17:45:23 -0700

    HelloUefi: Add 30 second stall (#120)
    ## Description
    
    This application is the simplest UEFI application possible, with no
    dependencies. These changes add a 30 second stall such that a platform
    may boot directly to this application and see "Hello UEFI!" with no need
    for a shell to run it (Such as ShellPkg). Futher This
    
    - [ ] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [X] Includes documentation?
      - readme.md
    
    ## How This Was Tested
    
    Ran on a physical machine, QemuQ35,
    
    built With GCC and VS2022
     
    ## Integration Instructions
    
    <_Describe how these changes should be integrated. Use N/A if nothing is
    required._>
    
    ---------
    
    Co-authored-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- OemPkg/HelloUefi/HelloUefi.c
+++ OemPkg/HelloUefi/HelloUefi.c
@@ -1,6 +1,6 @@
 /** @file
   This sample application that is the simplest UEFI application possible.
-  It simply prints "Hello Uefi!" to the UEFI Console Out device.
+  It simply prints "Hello Uefi!" to the UEFI Console Out device and stalls the CPU for 30 seconds.
 
   Copyright (C) Microsoft Corporation
   SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -8,6 +8,9 @@
 
 #include <Uefi.h>
 #include <Library/UefiApplicationEntryPoint.h>
+
+// 30 seconds in microseconds
+#define STALL_30_SECONDS 30000000
 
 /**
   The user Entry Point for Application. The user code starts with this function
@@ -30,14 +33,30 @@ UefiMain (
 {
   EFI_STATUS  Status;
 
-  if ((SystemTable == NULL) || (SystemTable->ConOut == NULL) || (SystemTable->ConOut->OutputString == NULL)) {
+  if (SystemTable == NULL) {
+
     return EFI_INVALID_PARAMETER;
   }
 
-  Status = SystemTable->ConOut->OutputString (SystemTable->ConOut, L"Hello Uefi!\r\n");
+  if ((SystemTable->ConOut == NULL) || (SystemTable->ConOut->OutputString == NULL) || (SystemTable->ConOut->ClearScreen == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  if ((SystemTable->BootServices == NULL) || (SystemTable->BootServices->Stall == NULL)) {
+    return EFI_INVALID_PARAMETER;
+  }
+
+  Status = SystemTable->ConOut->ClearScreen (SystemTable->ConOut);
   if (EFI_ERROR (Status)) {
     return Status;
   }
+
+  Status = SystemTable->ConOut->OutputString (SystemTable->ConOut, L"\r\nHello Uefi!\r\n");
+  if (EFI_ERROR (Status)) {
+    return Status;
+  }
+
+  SystemTable->BootServices->Stall (STALL_30_SECONDS);
 
   return EFI_SUCCESS;
 }
