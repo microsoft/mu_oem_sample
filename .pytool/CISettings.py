99f0314f7169e3b8d816109918a9139092ce4794
Aaron Pop <aaronpop@microsoft.com>
2026-09-04 14:30:50 -0700

    CI: Update dependencies for release/202608
    Update mu_basecore and mu_plus dependencies to release/202608.
    Refresh the Linux build container and replace the GCC
    variable group with explicit tool chain and VM image settings.
    
    Signed-off-by: Aaron Pop <aaronpop@microsoft.com>

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -6,17 +6,17 @@
 # SPDX-License-Identifier: BSD-2-Clause-Patent
 ##
 import glob
-import os
 import logging
+import os
+
+from edk2toolext import codeql as codeql_helpers
 from edk2toolext.environment import shell_environment
 from edk2toolext.invocables.edk2_ci_build import CiBuildSettingsManager
 from edk2toolext.invocables.edk2_ci_setup import CiSetupSettingsManager
+from edk2toolext.invocables.edk2_pr_eval import PrEvalSettingsManager
 from edk2toolext.invocables.edk2_setup import SetupSettingsManager
 from edk2toolext.invocables.edk2_update import UpdateSettingsManager
-from edk2toolext.invocables.edk2_pr_eval import PrEvalSettingsManager
 from edk2toollib.utility_functions import GetHostInfo
-
-from edk2toolext import codeql as codeql_helpers
 
 
 class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManager, PrEvalSettingsManager):
@@ -182,13 +182,13 @@ class Settings(CiSetupSettingsManager, CiBuildSettingsManager, UpdateSettingsMan
             {
                 "Path": "MU_BASECORE",
                 "Url": "https://github.com/Microsoft/mu_basecore.git",
-                "Branch": "release/202511",
+                "Branch": "release/202608",
                 "Recurse": {"CIFile": ".pytool/CISettings.py"}
             },
             {
                 "Path": "Common/MU",
                 "Url": "https://github.com/Microsoft/mu_plus.git",
-                "Branch": "release/202511"
+                "Branch": "release/202608"
             },
             {
                 "Path": "Features/MU_FEATURE_DFCI",
